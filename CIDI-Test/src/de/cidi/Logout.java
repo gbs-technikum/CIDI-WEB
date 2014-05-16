a780dae24efa7fb6380b5b45305dfc98399636ce
Robert <web@lala-rob.de>
2014-05-16 11:36:37 +0200

    Logout Class hinzugefügt

--- CIDI-Test/src/de/cidi/Logout.java
+++ CIDI-Test/src/de/cidi/Logout.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CLOSE extends HttpServlet{
-
-	private Controller c;
+public class Logout extends HttpServlet{
 	private DAO db;
 	private static final long serialVersionUID = 1L;
 	
@@ -17,11 +15,10 @@ public class CLOSE extends HttpServlet{
 	public void init() throws ServletException {
 		// TODO Auto-generated method stub
 		super.init();
-		c = Controller.getInstance();				//Controller Instance holen
 		db = Login.getMysql();
 	}
 
-	public CLOSE() {
+	public Logout() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,9 +27,8 @@ public class CLOSE extends HttpServlet{
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Login.getSession(request);
 		db.abmelden();								//SQL User abmelden
-		c.close();									//Verbindung trennen
+	
 		response.sendRedirect( "login.jsp" );		//weiterleitung an Login
 	}
 
